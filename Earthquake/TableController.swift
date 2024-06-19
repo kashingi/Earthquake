e78c2e305309f1b5d4f8edb21cd6be52f11dcd03
Kashingi <moriskashing74@gmail.com>
2024-06-19 15:19:34 +0300

    My initial commit

---
+++ Earthquake/TableController.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Earthquake
+//
+//  Created by EMTECH MAC on 19/06/2024.
+//
+
+import Foundation
---
+++ Earthquake/TableModel.swift
@@ -0,0 +1,8 @@
+//
+//  TableModel.swift
+//  Earthquake
+//
+//  Created by EMTECH MAC on 19/06/2024.
+//
+
+import Foundation
